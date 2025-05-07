0111f6def66f7372f149db43d7bd9b1e98a799de
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2025-05-07 15:55:38 +0530

    nav menu added with BTSRP and PHP
    partials folder and views folder with separate php files in root folder

---
+++ function.php
@@ -0,0 +1,15 @@
+<?php
+
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+  }
+  // dd($_SERVER['REQUEST_URI']);
+  
+  
+  function isCurrURL($url) {
+    return $_SERVER['REQUEST_URI'] === $url;
+  }
