6908210ea61c6a2abad6ca6e5e0081c9784d6af6
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2025-05-09 09:39:26 +0530

    connect to DB, config the file and fetch db data

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'your_db_host',     // e.g., url
+        'port'=> 'your_db_port',      // e.g., 0000
+        'dbname'=> 'your_db_name',    
+        'charset'=> 'your_charset',   // e.g., utf8mb4
+    ],
+];
--- index.php
+++ index.php
@@ -3,4 +3,18 @@
 require 'function.php';
 
 
-require 'routes.php';
+// require 'routes.php';
+
+require 'Database.php';
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+dd($posts);
+
+// foreach ($posts as $post) {
+//     echo $post['title'];
+// }   
